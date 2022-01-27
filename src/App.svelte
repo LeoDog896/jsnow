c79830f3f2af544aea53ad1c47abb58ae8c219a5
LeoDog896 <LeoDog896@hotmail.com>
2022-01-27 16:05:27 +0000

    Top-level await, codemirror

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,81 @@
 <script lang="ts">
 	import Tailwindcss from "./Tailwindcss.svelte"
+	import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup"
+	import { javascript } from "@codemirror/lang-javascript"
+	import { onMount } from "svelte"
+	import { ViewPlugin } from "@codemirror/view"
 
-	let value: string
+	let value: string = ""
+
+	const updatePlugin = ViewPlugin.fromClass(class {
+		constructor(view) {}
+
+		update(update) {
+			if (update.docChanged) value = update.state.doc.toString()
+		}
+	})
+
+
+	let editor: HTMLDivElement
+
+	onMount(() => {
+		let view = new EditorView({
+			state: EditorState.create({
+				extensions: [
+					basicSetup,
+					javascript(),
+					updatePlugin,
+					EditorView.lineWrapping,
+				],
+			}),
+			parent: editor
+		})
+	})
+
+
+	const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor
+
+	async function run(string: string): Promise<string> {
+		if (string == "") return ""
+		try {
+			const asyncFunction = AsyncFunction(string)
+			
+			const element = await asyncFunction({
+				log: (element) => alert(element)
+			})
+
+			if (Array.isArray(element)) {
+				return `Array(${element.length}) [${element.join(", ")}]`
+			}
+
+			if (typeof element == "object") {
+				return JSON.stringify(element)
+			}
+
+			if (typeof element == "string") {
+				return `"${element}"`
+			}
+
+			if (typeof element == "bigint") {
+				return `${element}n`
+			}
+
+			return element;
+		} catch(e) {
+			return e
+		}
+	}
 </script>
-<textarea bind:value={value}></textarea>
-<p>{eval(value)}</p>
+<div class="gap-0 grid grid-rows-1 grid-cols-2">
+	<div bind:this={editor}></div>
+	{#await run(value) then result}
+		<p>{result}</p>
+	{/await}
+</div>
 <Tailwindcss />
+<style>
+	:global(.cm-scroller) { 
+		overflow: scroll-y;
+		min-height: 100vh; 
+	}
+</style>
