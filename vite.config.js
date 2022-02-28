cd7efd592e74976a884634c366ea647f77dc6679
LeoDog896 <LeoDog896@hotmail.com>
2022-02-28 13:29:19 +0000

    Add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { VitePWA } from 'vite-plugin-pwa'
+import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
+	generateSitemap({ hostname: "https://now.js.org/", routes: ["/", "/index.html"] }),
     VitePWA({
       includeAssets: ['favicon.svg', 'robots.txt'],
       registerType: 'autoUpdate',
