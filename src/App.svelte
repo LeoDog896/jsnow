de9a21d9caf20dc15c5830c3fb65bf4fb519d82f
Tristan <LeoDog896@hotmail.com>
2022-02-28 15:48:48 -0500

    Import stylesheet from node_modules (prismjs)

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,8 @@
 	import Modal from 'svelte-simple-modal';
 	import Content from "./Content.svelte"
 	import { fade } from 'svelte/transition';
+	import "../node_modules/prismjs/themes/prism.css"
 </script>
-<svelte:head>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.22.0/themes/prism.min.css" rel="stylesheet" />
-</svelte:head>
 <Modal 
 	transitionBg={element => fade(element, { duration: 150 })}
 	transitionWindow={element => fade(element, { duration: 150 })}
